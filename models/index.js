bf87f3f3a7b5ce940980b77057d53199a9ca6932
udaantech <info@udaantechnologies.com>
2020-07-16 07:43:58 +0000

    changes in db

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 'user strict';
 const mongoose = require('mongoose');
 const config = require('../config');
-mongoose.connect(config.dbURI, { useNewUrlParser: true });
+mongoose.connect(config.dbURI, { 
+	useNewUrlParser: true,
+  	useUnifiedTopology: true,
+  	serverSelectionTimeoutMS: 5000
+});
 mongoose.set('useCreateIndex', true);
 mongoose.connection.on('error', function(err) {
 	if(err) throw err;
