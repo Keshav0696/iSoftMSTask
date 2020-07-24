302cc3d65e5ffc5c6ca97e3e0f0c5c6c11682ef4
keshavgarg <keshavgarg0696@gmail.com>
2020-07-24 12:39:36 +0530

    commit changes

--- routes/accessoriesRate.js
+++ routes/accessoriesRate.js
@@ -45,6 +45,40 @@ router.put('/editAccessoriesRate/:id', async function (req, res) {
   } 
 });
 
+
+router.put('/editPalletRate/:id', async function (req, res) {
+    if(req.body && req.params.id) {
+      var edited = await AccessoriesRate.findOneAndUpdate({ _id: req.params.id }, {
+        $set: {
+          fbaPallet : req.body
+        }
+      },
+        {
+          upsert: true
+        });
+      if (edited) {
+        res.send(edited);
+      } else {
+        res.status(500).send({status: 500, data: null, message: "Accessories are not available"});
+      }
+    } else {
+      res.status(500).json({ status: 500, data: null, message: "Please enter all required fields" });
+    }
+});
+
+router.get('/getPalletRate/:id', async function (req, res) {
+  if(req.params.id){
+  let found = await AccessoriesRate.findOne({_id: req.params.id})
+   if(found){
+      res.status(200).json(found);
+   }else{
+       res.status(500).json({ status: 500, data: null, message: "No data exist" });
+   }
+  }else{
+    res.status(500).json({ status: 500, data: null, message: "Rate id is required" });
+  }
+});
+
 router.get('/getAllAccessoriesRate', async function (req, res) {
 
     let found = await AccessoriesRate.find({})
